0466ec902f7673e08710ffc73d3b7d687b6c1e97
Nabayan Saha <nabayansaha@gmail.com>
2024-02-09 19:09:45 +0530

    updated Register

---
+++ src/Register/Register.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Register from "./Register";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Register />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
